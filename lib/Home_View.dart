13c3a63c71f7ff0b0b1dc5e4832b35813d5bc38f
Chathuranga97lj <chathuit97@gmail.com>
2022-12-20 23:52:53 +0530

    set first column

--- lib/Home_View.dart
+++ lib/Home_View.dart
@@ -16,7 +16,60 @@ class _BMIViewState extends State<BMIView> {
         title: Text('BMI Calculator'),
         centerTitle: true,
         backgroundColor: Color(0xFF04040C),
-        leading: Icon(Icons.menu),  // left side component >> items, right side >> actions
+        leading: Icon(
+            Icons.menu), // left side component >> items, right side >> actions
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Colors.white12,
+                    height: 170,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Icon(
+                          Icons.male,
+                          color: Colors.white,
+                          size: 80,
+                        ),
+                        Text(
+                          'MALE',
+                          style: TextStyle(color: Colors.white, fontSize: 25),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10,),
+                Expanded(
+                  child: Container(
+                    color: Colors.white12,
+                    height: 170,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Icon(
+                          Icons.female,
+                          color: Colors.white,
+                          size: 80,
+                        ),
+                        Text(
+                          'FEMALE',
+                          style: TextStyle(color: Colors.white, fontSize: 25),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
